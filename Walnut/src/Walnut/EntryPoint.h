a56c7e22bd06a6277bbd27d29913e99a2840bb46
Precise <3824793+Mr-Precise@users.noreply.github.com>
2022-08-21 00:46:31 +0300

    Add CMake build support!
    Add CMake build support!
    Fix build for Linux
    Todo: fix MinGW build

--- Walnut/src/Walnut/EntryPoint.h
+++ Walnut/src/Walnut/EntryPoint.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#ifdef WL_PLATFORM_WINDOWS
-
 extern Walnut::Application* Walnut::CreateApplication(int argc, char** argv);
 bool g_ApplicationRunning = true;
 
@@ -21,7 +19,7 @@ namespace Walnut {
 
 }
 
-#ifdef WL_DIST
+#ifdef _WIN32
 
 #include <Windows.h>
 
@@ -30,13 +28,9 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmd
 	return Walnut::Main(__argc, __argv);
 }
 
-#else
+#endif
 
 int main(int argc, char** argv)
 {
 	return Walnut::Main(argc, argv);
 }
-
-#endif // WL_DIST
-
-#endif // WL_PLATFORM_WINDOWS
